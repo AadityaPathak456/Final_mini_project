d4e0c85ecb32979a35c39b60f13e4b28addac5db
Aaditya Pathak <66768308+AadityaPathak456@users.noreply.github.com>
2024-01-02 11:30:31 +0530

    gfghfh

---
+++ script1.js
@@ -0,0 +1,16 @@
+
+// New code 
+  function login(event) {
+    event.preventDefault();
+    // Rest of the function
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    if (username === 'admin' && password === '12345') {
+      // Redirect to main.html if login is successful
+      window.location.href = 'main.html';
+    } else {
+      // Display an alert for unsuccessful login
+      alert('Invalid username or password');
+    }
+  }
